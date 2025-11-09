afc9ce61cf15030b42030400e5788bc06163a198
petir-be <kurtieepogii@gmail.com>
2025-11-09 11:12:19 +0800

    fetched some words

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Line from "./components/Line";
 
+const API_URL = 'api/api/fe/wordle-words';
 
 function App() {
   const [keypressed, setKeyPressed] = useState('');
+  const [word, setWord] = useState(null);
+  const fetchedRef = useRef(false);
    
+useEffect(() =>{
+  if(fetchedRef.current) return;
+  fetchedRef.current = true;
+
+  const fetchWord = async () =>{
+    try{
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      const randomWord = data[Math.floor(Math.random() * data.length)];
+      setWord(randomWord);
+    }catch(error){
+      console.error("Error fetching the word:", error);
+    }
+  };
+  fetchWord();
+}, []);
+
+
 
 useEffect(() =>{
   const handleKeyDown = (e) =>{
@@ -13,8 +34,6 @@ useEffect(() =>{
       setKeyPressed(keypressed.slice(0, -1));
       return;
     }
-
-    
     if(e.key.length !== 1){
       return;
     }else{
@@ -24,7 +43,6 @@ useEffect(() =>{
   
   window.addEventListener('keydown', handleKeyDown);
   
-
   return() => {
     window.removeEventListener('keydown', handleKeyDown)
   }
@@ -34,6 +52,7 @@ useEffect(() =>{
     <>
     <div className="text-6xl underline text-white min-h-dvh flex justify-center items-center">
         {keypressed}
+        {word}
     </div>
     </>
   )
